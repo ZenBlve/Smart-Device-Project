252ab2427f80b1f77a84fcc8cc813ac99d9992d1
anazari5 <artyn1530@gmail.com>
2026-03-30 02:24:40 -0700

    Update Main.java
    actually added code to main

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+public class Main {
+  public static void main(String[] args) {
+    SmartHomeManager manager = new SmartHomeManaer();
 
+    DoorLock lock = new DoorLock("DL", "Door Lock");
+    SmartLight light = new SmartLight("SL", "Light", 70);
+    Thermostat thermostat = new Thermostat("TS", "Thermostat", 72);
+    
+    manager.addDevice(lock);
+    manager.addDevice(light);
+    manager.addDevice(thermostat);
+    manager.displayAllStatuses();
+    
+    Controllable controllableDevice = light;
+    controllableDevice.turnOn();
+    
+    try {
+      manager.findDevice("N/A");
+    } catch (SmartHomeManager.DeviceNotFoundExcption e) {
+      System.out.println(e.getMessage());
+    }
+    
+    try {
+      manager.turnOnDevice("TS");
+    } catch (SmartHomeManager.UnsupportedDeviceOperationException e) {
+      System.out.println(e.getMessage());
+    }
+    
+    manager.displayAllStatuses();
+  }
+}
